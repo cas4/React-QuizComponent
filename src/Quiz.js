3fcd473da0c17e964ced2b0eb9294930c7bd2a3c
Andy Spears <spearscaiv@ornl.gov>
2019-06-14 13:04:27 -0400

    Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css'
 
-const Quiz = () => {
+const quizData = require('./quiz_data.json');
+
+class Quiz extends Component{
     //const [quiz_position, setQuiz_position] = useState(1);
+    constructor(props){
+        super(props)
+    }
 
-    const quizData = require('./quiz_data.json');
-
-    return (
-        <div>
-            <div className="QuizQuestion">
-                {quizData.quiz_questions[0].instruction_text}
+    render(){
+        return (
+            <div>
+                <div className="QuizQuestion">
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Quiz
